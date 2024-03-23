6a59c8d93ecd9880b8f7a477da629fd666018b41
Matheus R. dos Santos <matheus.ribeiro@sou.inteli.edu.br>
2024-03-23 00:12:12 -0300

    add new function

--- smart-contract/src/Contract.sol
+++ smart-contract/src/Contract.sol
@@ -5,8 +5,28 @@ contract Contract {
     mapping(address => bool) public users;
 
     address public owner;
+    address public assignee;
 
     constructor() {
         owner = msg.sender;
+    }
+
+    function assignTask(address _assignee) public {
+        require(msg.sender == owner, "Only owner can assign task");
+        assignee = _assignee;
+    }
+
+    function addNewUser(address _user) public {
+        require(msg.sender == owner, "Only owner can add new user");
+        users[_user] = true;
+    }
+
+    function getUser(address _user) public view returns (bool) {
+        return users[_user];
+    }
+
+    function removeUser(address _user) public {
+        require(msg.sender == owner, "Only owner can remove user");
+        users[_user] = false;
     }
 }

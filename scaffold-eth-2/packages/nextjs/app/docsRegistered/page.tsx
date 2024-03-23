8a8f2a5aade567e959cdae0cfdb017aec701c9e5
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-03-23 17:35:24 -0300

    Integração docsRegistered page

--- scaffold-eth-2/packages/nextjs/app/docsRegistered/page.tsx
+++ scaffold-eth-2/packages/nextjs/app/docsRegistered/page.tsx
@@ -1,21 +1,40 @@
-import React from "react";
-import DocumentCard from "~~/components/DocumentCard";
+"use client";
 
-const docsRegistered = () => {
-  const documents = ["Identidade", "Certidão", "CNH"];
+import React, { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import DocumentCard from "~~/components/DocumentCard";
+import { Address } from "~~/components/scaffold-eth";
+import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+
+
+const DocsRegistered = () => {
+  const { address: connectedAddress } = useAccount();
+
+  const [contracts, setContracts] = useState([]);
+
+  const { read, data, isLoading } = useScaffoldContractRead({
+    contractName: "YourContract", // Nome do seu contrato
+    functionName: "getContracts", // Nome da função do seu contrato
+    args: [connectedAddress], // Utilize o endereço diretamente
+  });
+
+  useEffect(() => {
+    if (data) {
+      setContracts(data); // Change the argument to parsedData
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Carregando contratos...</div>;
+  }
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <main className="p-6">
-        <h1 className="text-2xl font-bold mb-4">Documentos registrados</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {documents.map(doc => (
-            <DocumentCard key={doc} title={doc} />
-          ))}
-        </div>
-      </main>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {contracts.map((contract, index) => (
+        <DocumentCard key={index} title={contract.name} owner={contract.owner} />
+      ))}
     </div>
   );
 };
 
-export default docsRegistered;
+export default DocsRegistered;
--- scaffold-eth-2/packages/nextjs/contracts/deployedContracts.ts
+++ scaffold-eth-2/packages/nextjs/contracts/deployedContracts.ts
@@ -7,7 +7,7 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   11155111: {
     YourContract: {
-      address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+      address: "0x88f2bA8b39f90E79641BCC4608743ABC0dC52324",
       abi: [
         {
           inputs: [

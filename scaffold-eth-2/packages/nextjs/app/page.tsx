12333d8721b6260316cae6d4861a18475faffbdc
Heloisa Oliveira <heloisa.oliveira@sou.inteli.edu.br>
2024-03-23 20:44:21 -0300

    update: tela-home

--- scaffold-eth-2/packages/nextjs/app/page.tsx
+++ scaffold-eth-2/packages/nextjs/app/page.tsx
@@ -16,39 +16,14 @@ const Home: NextPage = () => {
         <section className="brand-section">
           <img className="logo" src="/logo.png" alt="Logo da ThinkLink" />
           <h1>ThinkLink</h1>
-          <p>Smart solutions for documents storage  <br /> and constrats signature.</p>
+          <p>Smart solutions for documents storage  <br /> and contracts signature.</p>
           <button className="access-documents"><Link href="./about"> ACCESS DOCUMENTS</Link></button>
         </section>
         <aside className="welcome-message">
           <h2>HELLO, WELCOME TO THINKLINK!</h2>
-          <p>Connected Address: <Address address={connectedAddress} /></p>
+          <div className="endereco"><p>Connected Address: <Address address={connectedAddress} /></p></div>
         </aside>
       </main>
-
-      <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
-        </div>
     </>
   );
 };

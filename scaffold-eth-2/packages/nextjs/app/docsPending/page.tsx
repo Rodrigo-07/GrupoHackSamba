8d9f2d53dc969882a17b94b04c99a1783e04acfc
Matheus Ribeiro <matheus.ribeiro@sou.inteli.edu.br>
2024-03-23 21:17:23 -0300

    add new funtion into front

--- scaffold-eth-2/packages/nextjs/app/docsPending/page.tsx
+++ scaffold-eth-2/packages/nextjs/app/docsPending/page.tsx
@@ -6,20 +6,20 @@ import DocumentCard from "~~/components/DocumentCard";
 import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
   
-const DocsRegistered = () => {
+const DocsPending = () => {
   const { address: connectedAddress } = useAccount();
 
-  const [contracts, setContracts] = useState([]);
+  const [contractsPeding, setContractsPeding] = useState([]);
 
   const { read, data, isLoading } = useScaffoldContractRead({
     contractName: "YourContract", // Nome do seu contrato
-    functionName: "getContracts", // Nome da função do seu contrato
+    functionName: "getPendentContracts", // Nome da função do seu contrato
     args: [connectedAddress], // Utilize o endereço diretamente
   });
 
   useEffect(() => {
     if (data) {
-      setContracts(data); // Change the argument to parsedData
+      setContractsPeding(data); // Change the argument to parsedData
     }
   }, [data]);
 
@@ -29,11 +29,11 @@ const DocsRegistered = () => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {contracts.map((contract, index) => (
+      {contractsPeding.map((contract, index) => (
         <DocumentCard key={index} title={contract.name} owner={contract.owner} />
       ))}
     </div>
   );
 };
 
-export default DocsRegistered;
+export default DocsPending;
